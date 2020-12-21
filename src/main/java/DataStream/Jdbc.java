13df16be4b186134842f303bf319e22a27e5ccde
kuhufu <huboemail@yeah.net>
2020-12-21 17:55:55 +0800

    update

---
+++ src/main/java/DataStream/Jdbc.java
@@ -0,0 +1,50 @@
+package DataStream;
+
+import org.apache.flink.api.common.eventtime.*;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.shaded.org.joda.time.DateTime;
+
+import java.time.Duration;
+
+public class Jdbc {
+    public static void main(String[] args) throws Exception {
+        var env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+//        env.getConfig().setAutoWatermarkInterval()
+
+        var tEnv = StreamTableEnvironment.create(env);
+
+
+        var dataStream = env.fromElements(
+                Tuple2.of(1, System.currentTimeMillis() + 1000),
+                Tuple2.of(1, System.currentTimeMillis() + 2000),
+                Tuple2.of(1, System.currentTimeMillis() + 3000))
+                .assignTimestampsAndWatermarks(WatermarkStrategy
+                        .<Tuple2<Integer, Long>>forBoundedOutOfOrderness(Duration.ofSeconds(3))
+                        .withTimestampAssigner((event, ts) -> event.f1)
+                )
+                .addSink(JdbcSink.sink(
+                        "INSERT INTO person(name, amount) VALUES(?, ?)",
+                        (ps, v) -> {
+                            ps.setString(1, String.valueOf(v.f0));
+                            ps.setInt(2, Math.toIntExact(v.f1));
+                        },
+                        new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                                .withUrl("jdbc:mysql://123.57.139.215:3316/flinkdb")
+                                .withDriverName("com.mysql.jdbc.Driver")
+                                .withUsername("root")
+                                .withPassword("onekick123456")
+                                .build()
+                ));
+
+        env.execute("jdbc");
+    }
+}
--- src/main/java/DataStream/Kafka.java
+++ src/main/java/DataStream/Kafka.java
@@ -1,19 +1,11 @@
 package DataStream;
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.common.typeinfo.Types;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
-import org.apache.flink.util.Collector;
 
 import java.util.Properties;
-
-import static org.apache.flink.table.api.Expressions.$;
 
 public class Kafka {
     public static void main(String[] args) throws Exception {
